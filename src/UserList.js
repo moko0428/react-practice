e2454d34a51453f21d5ef558f02f5641c9091b9b
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-04 19:06:02 +0900

    deps에 특정 값 넣기 : deps에 특정 값을 넣지 않게 되면 리렌더링 될 때 최신 props 상태를 가르키지 않고 deps를 넣지 않으면 리렌더링 될 때마다 모든 값을 호출하고 deps에 값을 넣으면 특정 값만 바뀔 때마다 호출 된다.

--- src/UserList.js
+++ src/UserList.js
@@ -2,11 +2,8 @@ import React, { useEffect } from "react";
 
 const User = ({ user, onRemove, onToggle }) => {
   useEffect(() => {
-    console.log("컴포넌트가 화면에 나타남");
-    return () => {
-      console.log("컴포넌트가 화면에서 사라짐");
-    };
-  }, []);
+    console.log(user);
+  }, [user]);
   return (
     <div>
       <b
