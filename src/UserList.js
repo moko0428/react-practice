728db450d48f82655713e0bf1abfdd7fa5d41d53
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-04 18:57:33 +0900

    useEffect를 사용하여 마운트/언마운트/업데이트시 할작업 설정하기 : 마운트 시 하는 작업 1. props로 받은 값을 컴포넌트의 로컬 상태로 설정 2. 외부 API요청 (REST API 등) 3. 라이브러리 사용 (D3, Video.js 등) 4. setInterval을 통한 반복 작업 혹은 setTimeout을 통한 작업 예약 언마운트 시에 작업 1. setInterval, setTimeout을 사용하여 등록한 작업들 clear하기 (clearInterval, clearTimeout) 2. 라이브러리 인스턴스 제거

--- src/UserList.js
+++ src/UserList.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const User = ({ user, onRemove }) => {
+const User = ({ user, onRemove, onToggle }) => {
+  useEffect(() => {
+    console.log("컴포넌트가 화면에 나타남");
+    return () => {
+      console.log("컴포넌트가 화면에서 사라짐");
+    };
+  }, []);
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{ cursor: "pointer", color: user.active ? "green" : "black" }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 };
-const UserList = ({ users, onRemove }) => {
+const UserList = ({ users, onRemove, onToggle }) => {
   return (
     <>
       <div>
         {users.map((user) => (
-          <User user={user} key={user.id} onRemove={onRemove} />
+          <User
+            user={user}
+            key={user.id}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          />
         ))}
       </div>
     </>
