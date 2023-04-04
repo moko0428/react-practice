3f48dcbef14fa92ac9126065555905e8d4167bf9
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-04 17:47:16 +0900

    onchage와 값이 안바뀌는 오류 해결 : input 태그에 name 속성을 주지 않아서 오류가 낫던것..

--- src/InputSample.js
+++ src/InputSample.js
@@ -22,8 +22,13 @@ const InputSample = () => {
   };
   return (
     <div>
-      <input placeholder="이름" onChange={onChange} value={name} />
-      <input placeholder="닉네임" onChange={onChange} value={nickName} />
+      <input placeholder="이름" onChange={onChange} value={name} name="name" />
+      <input
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickName}
+        name="nickName"
+      />
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값:</b>
