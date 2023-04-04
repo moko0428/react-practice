ad67590d5e2ed92d4c47e25886af423cf8728868
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-04 20:02:56 +0900

    마무리 멘트

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useCallback, useMemo, useRef, useState } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 
+//리액트 개발할 땐 useCallback, useMemo, React.memo는 컴포넌트의 성능을 실제로 개선할 수 있는 상황에서만 사용.
+// ex) User 컴포넌트에 b와 button에 onClick 으로 설정해준 함수들은 해당 함수들을 useCallback으로 재사용한다고 해서 리렌더링을 막을 수 있는 것은 아니므로, 굳이 그렇게 할 필요는 없다.
+// 렌더링 최적화를 하지 않을 컴포넌트에 React.memo를 사용하는 것은, 불필요한 props 비교만 하는 것이기 때문에 실제로 렌더링을 방지할 수 있는 상황이 있는 경우에만 사용 권장.
+
 const countActiveUsers = (users) => {
   console.log("활성 사용자 수를 세는중 ...");
   return users.filter((user) => user.active).length;
--- src/UserList.js
+++ src/UserList.js
@@ -34,5 +34,10 @@ const UserList = ({ users, onRemove, onToggle }) => {
     </>
   );
 };
-
-export default React.memo(UserList);
+// 추가적으로, React.memo에서 두번째 파라미터에 propsAreEqual 이라는 함수를 사용하여 특정 값들만 비교를 하는 것도 가능하다.
+// 하지만 잘못 사용한다면 오히려 의도치 않은 버그들이 발생하기 쉽다.
+// ex) 함수형 업데이트로 전환을 안했는데, 이렇게 users만 비교를 하게 된다면 onToggle과 onRemove에서 최신 users 배열을 참조하지 않으므로 심각한 오류가 발생할 수 있다.
+export default React.memo(
+  UserList,
+  (prevProps, nextProps) => prevProps.users === nextProps.users
+);
